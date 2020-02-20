6bb1d9e62a63de9b8a8939b4a7b10a8252287210
Damiancie24 <d.cieszynski@microsoft.wsei.edu.pl>
2020-02-20 19:18:24 +0100

    Wersja Monolityczna

---
+++ Zgadywanka/ConsoleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra za Duzo za Malo");
+
+            //1. Komputer losuje
+            Random los = new Random(); // Tworze obiekt tupu random
+
+            int wylosowana = los.Next(1, 101);
+
+            Console.WriteLine(wylosowana);
+
+            bool odgadniete = false;
+
+            // Dopuki nie odgadniete
+            while(!odgadniete)
+            {
+                //2. człowiek proponuje 
+                Console.WriteLine("Wylosowałem liczbę od 1 - 100. \nOdgadnij ją!");
+
+                Console.Write("Podaj swoją propozycje");
+
+                //int propozycja = int.Parse(Console.ReadLine());
+                int propozycja = int.Parse(Console.ReadLine());
+
+
+                //3. komp ocenia
+
+               // while (propozycja > wylosowana || propozycja < wylosowana)
+               // {
+
+
+                    if (propozycja < wylosowana)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Za mało!");
+                        Console.ResetColor();
+                    }
+                    else if (propozycja > wylosowana)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Za duzo!");
+                        Console.ResetColor();
+
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Trafiono!");
+                        Console.ResetColor();
+                        odgadniete = true;
+
+                    }
+
+                   // Console.Write("Podaj swoją propozycje jeszcze raz");
+
+                   // propozycja = int.Parse(Console.ReadLine());
+
+
+               // }
+
+               // Console.ForegroundColor = ConsoleColor.Green;
+                //Console.WriteLine("Trafiono!");
+                //Console.ResetColor();
+
+            }
+
+            Console.WriteLine("Koniec Gry Wygrałes");
+
+        }
+    }
+}
